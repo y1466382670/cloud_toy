058b78ba3eee0ce678266f3358029d6c39b7aaf1
yanzhiyu <1466382670@qq.com>
2020-06-02 16:51:22 +0800

    init

--- cloud_toy_gateway/src/main/java/com/yu/CloudToyGatewayApplication.java
+++ cloud_toy_gateway/src/main/java/com/yu/CloudToyGatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class CloudToyGatwayApplication {
+public class CloudToyGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CloudToyGatwayApplication.class, args);
+        SpringApplication.run(CloudToyGatewayApplication.class, args);
     }
 
 }
